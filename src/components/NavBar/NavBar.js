cbe04cbfd775cdb0e066ec19ebb59cfe5dbb85d1
Juan Ignacio Otaño <juan_io@hotmail.com>
2023-03-30 21:56:32 -0300

    agrego navbar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <h3>PLACANORTE</h3>
+      <div>
+        <button>Placas</button>
+        <button>Cantos</button>
+        <button>Herrajes</button>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
